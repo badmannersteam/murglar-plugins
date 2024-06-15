d83a6df2e60d3116ba58a3f881629f48916e4370
badmannersteam <badmannersteam@gmail.com>
2024-06-16 00:03:33 +0200

    Next version: 4.3-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
             google()
         }
         create("libs") {
-            version("murglar-plugins", "4.2")
+            version("murglar-plugins", "4.3-SNAPSHOT")
 
             // 1.5+ has this - https://github.com/Kotlin/kotlinx.serialization/issues/2231
             version("kotlinx-json", "1.4.1")
