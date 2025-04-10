17f80ea96f79ab73dfa3a8c164de203ac18139b4
badmannersteam <badmannersteam@gmail.com>
2025-04-11 00:21:20 +0200

    Added MP4 container.

--- core/src/main/kotlin/com/badmanners/murglar/lib/core/model/track/source/Container.kt
+++ core/src/main/kotlin/com/badmanners/murglar/lib/core/model/track/source/Container.kt
@@ -6,9 +6,32 @@ package com.badmanners.murglar.lib.core.model.track.source
  */
 enum class Container {
 
+    /**
+     * Pure progressive stream, like generic MP3/FLAC/AAC/WAV/etc file.
+     */
     PROGRESSIVE,
 
+    /**
+     * MPEG-4 Part 14 container file.
+     *
+     * Corresponding [Source.extension] must be set to the actual audio track format, contained in the file,
+     * because client may demux the audio track from the container.
+     */
+    MP4,
+
+    /**
+     * HLS stream.
+     *
+     * Corresponding [Source.extension] must be set to the actual audio track format, contained in the file,
+     * because client may demux the audio track from the container.
+     */
     HLS,
 
+    /**
+     * DASH stream.
+     *
+     * Corresponding [Source.extension] must be set to the actual audio track format, contained in the file,
+     * because client may demux the audio track from the container.
+     */
     DASH
 }
