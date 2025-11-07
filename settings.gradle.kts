1034e48c63a51199f7313dea7647355ab49f03db
badmannersteam <badmannersteam@gmail.com>
2025-11-07 21:33:09 +0100

    Next version: 6.2-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
             google()
         }
         create("libs") {
-            version("murglar-plugins", "6.1")
+            version("murglar-plugins", "6.2-SNAPSHOT")
 
             version("kotlin", "2.2.21")
             version("android-build-plugin", "8.10.1")
